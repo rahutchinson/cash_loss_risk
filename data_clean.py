8d4bf8f3c1dff6639fd6cb54afea2f9c16de88ac
Ryan Hutchinson <ryanahutchinson@gmail.com>
2019-06-10 20:51:21 -0500

    working on a dao

--- data_clean.py
+++ data_clean.py
@@ -1,1 +1,22 @@
 # Clean some data
+import pandas as pd
+import os
+import datetime as dt
+
+env = os.getattr("RUNTIME")
+
+if env == 'local':
+    # amex
+    df_amex = pd.read_csv("gdrive/My Drive/Colab_Notebooks/AmexNew2019-5.csv", header=None, usecols=[0,7])
+    # chase
+    df_chase = pd.read_csv('/content/gdrive/My Drive/Colab_Notebooks/chase_new.CSV', error_bad_lines=False)
+    # amazon
+    df_amazon = pd.read_csv('/content/gdrive/My Drive/Colab_Notebooks/Amazon.csv')
+    
+if env == 'gcp_function':
+    # TODO
+    raise Exception("Data Cleansing not implement for GCP")
+    
+
+
+    
